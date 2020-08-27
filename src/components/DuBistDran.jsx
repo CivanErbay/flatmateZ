8a92c4460695427e83fdf21a06a5ba5cf93b7289
navic1992 <civan.erbay@web.de>
2020-08-27 11:50:15 +0200

    Add Weekly Calculation

--- src/components/DuBistDran.jsx
+++ src/components/DuBistDran.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Box from "@material-ui/core/Box";
 import {makeStyles} from "@material-ui/core/styles";
 
@@ -15,14 +15,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const DuBistDran = () => {
-    //Here weekly Calculation --> Next and Before button
 
+    const [currentPutzperson, setCurrentPutzperson] = useState("")
     const mitglieder = ["Paul", "Eyleen", "Anthea", "Civan", "Paco"]
 
-    const classes =useStyles();
+
+    useEffect (() => {
+        let now = new Date();
+        let onejan = new Date(now.getFullYear(), 0, 1);
+        let week = Math.ceil( (((now - onejan) / 86400000) + onejan.getDay() + 1) / 7 );
+
+        setCurrentPutzperson(mitglieder[week%mitglieder.length])
+
+    }, [])
+
+    const classes = useStyles();
     return (
         <Box className={classes.center} style={{height: "100vh"}}>
-            <h1 className={classes.text}>Eyleen</h1>
+            <h1 className={classes.text}>{currentPutzperson}</h1>
         </Box>
     )
 }
